380fdc431e9c17567a38419d2b99a222cc366fef
Fyrik <zr7@students.waikato.ac.nz>
2019-06-03 19:20:50 +1200

    Events on Keyup and passing values from a textbox.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -5,6 +5,8 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.css'],
   template:`
+    <input class="inputBox" (keyup.enter)="onKeyUp($event)"/> <br/>
+    <input class="inputBox" #text (keyup.enter)="onKeyUp2(text.value)">
     <div (click)="onDivClick()">
     <button 
       class="btn btn-primary" 
@@ -25,12 +27,21 @@ export class CourseComponent{
   isActive = false;
   colSpan = 2;
 
+  onKeyUp($event) {
+    console.log("Enter was pressed: ", $event.target.value);
+  }
+
+  onKeyUp2(text) {
+    console.log("Enter2 was pressed: ", text);
+  }
+
   onDivClick() {
     console.log("Div was clicked.");
   }
 
   onSave($event) {
     $event.stopPropagation();
+    this.isActive = true;
     console.log("Button was clicked.", $event);
   }
   constructor() { }
