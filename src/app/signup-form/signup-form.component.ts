5fcf2937014fe05b03ed13f06f505ff14b568eed
Fyrik <zr7@students.waikato.ac.nz>
2019-07-14 23:18:10 +1200

    Upgraded dependencies; Nested control;

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -11,10 +11,12 @@ import { passwordValidators } from './password.validators';
 export class SignupFormComponent {
   form = new FormGroup(
     {
-      username: new FormControl('', 
-            [Validators.required,Validators.minLength(3), UsernameValidators.cannotContainSpace], 
-            UsernameValidators.shouldBeUnique),
-      password: new FormControl('', [Validators.required, passwordValidators.cannotContainDot])
+      account: new FormGroup({
+        username: new FormControl('', 
+        [Validators.required,Validators.minLength(3), UsernameValidators.cannotContainSpace], 
+        UsernameValidators.shouldBeUnique),
+        password: new FormControl('', [Validators.required, passwordValidators.cannotContainDot])
+      })
     }
   );
 
@@ -25,10 +27,10 @@ export class SignupFormComponent {
   }
 
   get username() {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 
   get password() {
-    return this.form.get('password');
+    return this.form.get('account.password');
   }
 }
