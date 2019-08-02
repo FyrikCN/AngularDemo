21deb761bf3c6a712f24603e182865d86c13c93b
Fyrik <zr7@students.waikato.ac.nz>
2019-08-02 14:07:09 +1200

    Http Patch and Delete method; onInit interface.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -6,26 +6,43 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
-  posts;
+export class PostComponent implements OnInit{
+  posts: any;
   private url = 'http://jsonplaceholder.typicode.com/posts';
 
   constructor(private http: HttpClient) {
-    http.get(this.url).subscribe(response => {
+  }
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe(response => {
       this.posts = response;
       console.log(response);
     });
-
   }
 
   createPost(input: HTMLInputElement) {
-    let post = { title: input.value };
+    let post: any = { title: input.value };
     input.value = '';
     this.http.post(this.url, JSON.stringify(post))
       .subscribe(response => {
-        post['id'] = response['id'];
+        post.id = response['id'];
         this.posts.splice(0, 0, post);
         console.log(post);
+      })
+  }
+
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post['id'], JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response);
+      })
+  }
+
+  deletePost(post) {
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        this.posts.splice(index, 1);
       })
   }
 }
